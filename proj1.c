da1fd766c2c64e994e547cec03734333bd9689e3
Thao Nguyen <fernthao@Thaos-MacBook-Air.local>
2025-09-13 20:13:45 -0400

    Fix bug - byte order

--- proj1.c
+++ proj1.c
@@ -5,6 +5,7 @@
 
 #define ARG_PRINT  0x1
 #define ARG_SUMMARY   0x2
+#define ADDR_LEN 4  // IPv4 address length in bytes
 
 unsigned short cmd_line_flags = 0;
 char * filename = NULL;
@@ -35,9 +36,9 @@ void parseargs (int argc, char *argv [])
             case 'r':
                 filename = optarg;
                 break;
-            case '?': // TODO why is this needed?
             default:
-              usage (argv [0]);
+                fprintf (stderr,"error: unknown option -%c\n",opt);
+                usage (argv [0]);
         }
     }
     if (cmd_line_flags == 0)
@@ -49,10 +50,46 @@ void parseargs (int argc, char *argv [])
 
 void printMode (char * filename)
 {
+    unsigned char buffer[ADDR_LEN]; 
+    FILE * file = fopen(filename, "rb");
+
+    if (file == NULL) {
+        fprintf(stderr, "error: could not open file %s\n", filename);
+        exit(1);
+    }
+    // Read and process the file in print mode
+    while (fread(&buffer, sizeof(buffer), 1, file) == 1) {
+        // Extract quads
+        printf("%u.%u.%u.%u\n", buffer[0], buffer[1], buffer[2], buffer[3]);
+    }
+    fclose(file);
 }
 
+// Summary mode reports the total number of IPv4 addresses in the input file, as well as the number of addresses from the
+// 10.0.0.0/8 private address range. The output will consist of two lines, as follows:
+// total IPs: X
+// private IPs: Y
+// The “X” is the total number of IPv4 addresses in the file. The “Y” is the number from the 10.0.0.0/8 address
+// range—i.e., IPv4 addresses whose first quad is 10
 void summaryMode (char * filename)
 {
+    unsigned char buffer[ADDR_LEN];
+    FILE * file = fopen(filename, "rb");
+    if (file == NULL) {
+        fprintf(stderr, "error: could not open file %s\n", filename);
+        exit(1);
+    }
+    int total_ips = 0;
+    int private_ips = 0;
+    while (fread(&buffer, sizeof(buffer), 1, file) == 1) {
+        total_ips++;
+        if (buffer[0] == 10) {
+            private_ips++;
+        }
+    }
+    fclose(file);
+    printf("total IPs: %d\n", total_ips);
+    printf("private IPs: %d\n", private_ips);
 }
 
 int main (int argc, char *argv [])
@@ -60,7 +97,7 @@ int main (int argc, char *argv [])
     parseargs (argc,argv);
 
     // Check that a filename was provided
-    if (filename == NULL || filename == "")
+    if (filename == NULL)
     {
         fprintf (stderr,"error: no filename provided\n");
         usage (argv [0]);
